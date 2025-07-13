a145f21aa574fca6c63cb0597e4d8c28107e992d
Rushikesh Ghosalkar <rushighosalkar@gmail.com>
2025-07-13 13:40:43 +0530

    moved work experience section to different file

--- exportToPDF.js
+++ exportToPDF.js
@@ -1,12 +1,24 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
+const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
+  const indexPath = path.join(__dirname, 'index.html');
+  const workExpPath = path.join(__dirname, 'workExperienceForMicrosoftApplication.html');
+
+  let indexHtml = fs.readFileSync(indexPath, 'utf8');
+  const workExpHtml = fs.readFileSync(workExpPath, 'utf8');
+
+  indexHtml = indexHtml.replace(
+    '<div id="work-experience-container"></div>',
+    `<div id="work-experience-container">${workExpHtml}</div>`
+  );
   // Load local HTML file
-  const filePath = path.resolve(__dirname, 'index.html');
+  const filePath = path.resolve(__dirname, 'temp.html');
+  fs.writeFileSync(filePath, indexHtml);
   await page.goto(`file://${filePath}`, { waitUntil: 'networkidle0' });
 
   // Generate PDF
